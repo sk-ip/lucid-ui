5401d205cb261b6657c83de9a7f3daccf74ec04b
Sudhanshu Kumar <sudhanshukumar5459@gmail.com>
2022-02-19 23:07:05 +0530

    add modal component

---
+++ component/modal/modal.js
@@ -0,0 +1,14 @@
+function ModalModule() {
+    const modalClose = document.querySelectorAll('.modal-close')
+
+    function hideModal(e) {
+        console.log(e)
+        e.preventDefault();
+        e.target.parentElement.parentElement.style.visibility = 'hidden';
+    }
+
+    modalClose.forEach((modalCloseIcon) => modalCloseIcon.addEventListener('click', hideModal))
+
+}
+
+export default ModalModule
